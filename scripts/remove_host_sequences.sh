a36407d4841fa208774a177fc3a36331e3327505
Amos Raphenya <raphenar@mcmaster.ca>
2020-04-13 16:17:40 -0400

    replaced bowtie2 with hisat2

--- scripts/remove_host_sequences.sh
+++ scripts/remove_host_sequences.sh
@@ -1,13 +1,13 @@
 # example: ./remove_host_sequences.sh Wuhan1-LN
 SAMPLE=$1
 
-# build db 
-bowtie2-build --threads 25 MN908947_3.fasta sars-cov-2
+# build db
+hisat2-build -p 25 MN908947_3.fasta sars-cov-2
 
 # align reads to reference
 # bowtie2 mapping against host sequence database, keep both mapped and unmapped reads (paired-end reads)
 # please note that ${SAMPLE}-R1_paired.fastq.gz and ${SAMPLE}-R2_paired.fastq.gz should be from after merging, trimming, and qc
-bowtie2 -x sars-cov-2 --threads 20 -1 ${SAMPLE}-R1_paired.fastq.gz -2 ${SAMPLE}-R2_paired.fastq.gz  -S  ${SAMPLE}_mapped_and_unmapped.sam
+hisat2 -x sars-cov-2 --threads 20 -1 ${SAMPLE}-R1_paired.fastq.gz -2 ${SAMPLE}-R2_paired.fastq.gz  -S  ${SAMPLE}_mapped_and_unmapped.sam
 
 # convert file .sam to .bam
 samtools view --threads 20 -bS ${SAMPLE}_mapped_and_unmapped.sam > ${SAMPLE}_mapped_and_unmapped.bam
