a659b57cb4206ff2aaa89a21f40e484c78e3ee7e
Jalees A. Nasir <60946013+jaleezyy@users.noreply.github.com>
2020-04-26 16:40:32 -0400

    added comments

--- scripts/primer_file_generation/generate_primer_files.py
+++ scripts/primer_file_generation/generate_primer_files.py
@@ -1,5 +1,11 @@
 #!/bin/python
 
+### Generate primer FASTA files (all, forward only and reverse only) given a .tsv
+### .tsv assumed to have structure based on ARTIC primer schemes (example: https://github.com/artic-network/artic-ncov2019/blob/master/primer_schemes/nCoV-2019/V3/nCoV-2019.tsv)
+### At minimum, .tsv is expected to be 3 columns: name, (any information, i.e. pool), sequence)
+
+### TODO: tweak names based on ARTIC 'alt' appends to certain primer names
+
 import sys
 from Bio import SeqIO
 
@@ -9,6 +15,7 @@ except IndexError:
 	print("Single argument required leading to a .tsv file")
 	exit()
 
+# Generate fasta file from given .tsv (containing all primers)
 output = str(input).rsplit(".", 1)[0].rsplit("/",1)[0]
 
 with open(output+".fasta", 'w+') as out:
@@ -16,6 +23,9 @@ with open(output+".fasta", 'w+') as out:
 		next(infile)
 		for line in infile:
 			out.write(">" + str(line).split("\t")[0] + "\n" + str(line).split("\t")[2] + "\n")
+
+# Create split fasta files based on direction (i.e. left/forward or right/reverse)
+### TODO: more flexible considerations for directionality (ex. .tsv provided '+' or '-') 
 
 fw = open(output+"_fw.fasta", 'w+')
 rc = open(output+"_rc.fasta", 'w+')
