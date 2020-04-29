4b7046aaa0b7ef2b577b012e4ae148e0dcd8d95e
Jalees A. Nasir <60946013+jaleezyy@users.noreply.github.com>
2020-04-29 10:18:24 -0400

    added intermediate file cleanup

--- scripts/kraken2-build.sh
+++ scripts/kraken2-build.sh
@@ -5,8 +5,11 @@ kraken2-build --download-taxonomy --db db --threads 20
 kraken2-build --download-library viral --db db --threads 20
 #kraken2-build --download-library human --db db --threads 20
 
-# clean before building db
+## clean before building db
 rm db/hash.k2d  db/opts.k2d  db/seqid2taxid.map  db/taxo.k2d
 
 ## build
 kraken2-build --build --threads 20 --db db
+
+## clean intermediate files in db
+kraken2-build --clean --threads 20 --db db
