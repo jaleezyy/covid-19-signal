a898f9967dea3c0687c0af17d5e5caa4e74c5562
Jalees A. Nasir <60946013+jaleezyy@users.noreply.github.com>
2020-05-04 20:02:05 -0400

    added regex to pull more specific R1 and R2 reads

--- generate_sample_table.sh
+++ generate_sample_table.sh
@@ -41,8 +41,8 @@ fi
 
 for file in $database_dir/*.fastq*; do
 	sample=$(basename $file | cut -d_ -f 1)
-	r1=$(ls $database_dir/$(basename $file | cut -d_ -f1)*R1*)
-	r2=$(ls $database_dir/$(basename $file | cut -d_ -f1)*R2*)
+	r1=$(ls $database_dir/$(basename $file | cut -d_ -f1)*R1* | grep ${sample}[^0-9])
+	r2=$(ls $database_dir/$(basename $file | cut -d_ -f1)*R2* | grep ${sample}[^0-9])
 	echo ${sample},${r1},${r2} >> .${name} && echo ${sample},${r1},${r2} 
 done
 
