a40a79275c1e7575943c90e6ce1c4d717c24639c
Jalees A. Nasir <jalees_nasir@hotmail.com>
2020-05-13 16:17:15 -0400

    added summary count option

--- resources/dev_scripts/primer_finder.py
+++ resources/dev_scripts/primer_finder.py
@@ -39,12 +39,30 @@ def main(args):
 	counter(args.forward_primers, "R2", out)
 	counter(args.reverse_primers, "R1", out)
 	out.close()
+	if args.summary is True:
+		R1 = 0
+		R2 = 0
+		Unknown = 0
+		with open(str(args.output).rsplit(".",1)[0] + ".summary", 'w+') as out:
+			for line in open(args.output):
+				if "R1" in line.split("\t")[2]:
+					R1+=int(line.split("\t")[1])
+				elif "R2" in line.split("\t")[2]:
+					R2+=int(line.split("\t")[1])
+				else:
+					Unknown+=int(line.split("\t")[1])
+			out.write("R1: %s\nR2: %s\n" % (str(R1), str(R2)))
+			print("\nR1: %s\nR2: %s\n" % (str(R1), str(R2)))
+			if Unknown > 0 :
+				out.write("Unknown: " + str(Unknown))
+				print("Unknown: " + str(Unknown))
 
 def run():
 	parser = argparse.ArgumentParser(description='Count the instances of primers from given forward and reverse primer FASTAs on FASTQs within the current working directory.')
 	parser.add_argument('-1','--forward_primers', dest="forward_primers", default=None, required=True, help='fasta forward primers file')
 	parser.add_argument('-2','--reverse_primers', dest="reverse_primers", default=None, required=True, help='fasta reverse primers file')
 	parser.add_argument('-o','--output', dest="output", default="count.txt", required=False, help='output file name. "count.txt" is the default.')
+	parser.add_argument('--summary', dest="summary", action="store_true", help='output summary counts for forward and reverse primers in separate file')
 	args = parser.parse_args()
 	main(args)  
 
