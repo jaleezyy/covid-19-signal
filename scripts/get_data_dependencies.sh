89306fdedf60b18e5c1ce14ae85ac0faf261a8e2
Finlay Maguire <finlaymaguire@gmail.com>
2020-05-22 01:49:53 -0300

    Add human reference to data_dep script

--- scripts/get_data_dependencies.sh
+++ scripts/get_data_dependencies.sh
@@ -49,3 +49,6 @@ kraken2-build --download-library viral --db $database_dir/Kraken2/db --threads 1
 kraken2-build --build --threads 10 --db $database_dir/Kraken2/db
 kraken2-build --clean --threads 10 --db $database_dir/Kraken2/db
 
+# get the GRCh38 BWA index human genome
+curl -s "ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/001/405/GCA_000001405.15_GRCh38/seqs_for_alignment_pipelines.ucsc_ids/GCA_000001405.15_GRCh38_no_alt_plus_hs38d1_analysis_set.fna.bwa_index.tar.gz" > $database_dir/GRCh38_bwa.tar.gz
+tar xvf $database_dir/GRCh38_bwa.tar.gz -C $database_dir
