7eb30b64d0f2d93e394872b938761fa9f4d15969
Jalees A. Nasir <jalees_nasir@hotmail.com>
2021-03-01 15:42:43 +0000

    update file handling to count needed lines per sample in final sample table

--- resources/dev_scripts/generate_sample_table_legacy.sh
+++ resources/dev_scripts/generate_sample_table_legacy.sh
@@ -11,22 +11,26 @@ name="sample_table.csv"
 existing=0
 
 HELP="""
-ASSUMES FASTQ FILES ARE NAMED AS <sample_name>_L00#_R{1,2}*.fastq(.gz)
+ASSUMES FASTQ FILES ARE NAMED AS <sample_name>_*_R1_*.fastq(.gz)
 
 Flags:
-    -d  :  Path to directory containing sample fastq(.gz) files (Absolute paths preferred for consistency, but can use relative paths)
+    -d  :  Path to directory containing individual sample fastq(.gz) (Absolute paths preferred for consistency, but can use relative paths)
+    -p  :  Path to directory containing pooled samples (i.e., 4 FASTQs per sample: L001_R1, L001_R2, L002_R1, L002_R2)
     -n  :  Name of final sample table (with extension) (default: 'sample_table.csv') - will overwrite if file exists
     -e  :  File path to an existing sample table - will append to the end of the provided table
 
 Select one of '-n' (new sample table) or '-e' (existing sample table). 
 If neither provided, a new sample table called 'sample_table.csv' will be created (or overwritten) by default. 
+
+Can specify both -d and -p and the generated table will combine both sets of data. It is best to separate the pooled samples from individual samples to avoid confusion. 
 """
 
-while getopts ":d:n:e:" option; do
+while getopts ":d:n:e:p:" option; do
     case "${option}" in
         d) database_dir=$OPTARG;;
         n) name=$OPTARG;;
         e) existing=$OPTARG;;
+        p) pooled_dir=$OPTARG;;
     esac
 done
 
@@ -53,19 +57,32 @@ fi
 
 if [ ! $database_dir = 0 ]; then
 	samples_dir=()
-	echo -e "Adding samples"
+	echo -e "Adding individual samples"
 	for file in $database_dir/*.f?(ast)q*; do
 		sample=$(basename $file | cut -d_ -f 1)
-		count=$(($(ls $database_dir/${sample}*_L00*_R{1,2}*.f?(ast)q* | wc -l)/2)) # estimate # of files
 		if [[ ! " ${samples_dir[@]} " =~ " ${sample} " ]]; then
-			for (( i=1; i<=$count; i++ )); do
-				r1=$(ls $database_dir/${sample}*_L00${i}_R1* | grep /${sample}_)
-				r2=$(ls $database_dir/${sample}*_L00${i}_R2* | grep /${sample}_)
-				echo ${sample},${r1},${r2} >> ${name} && echo ${sample},${r1},${r2}
-			done
+			r1=$(ls $database_dir/$(basename $file | cut -d_ -f1)*R1* | grep /${sample}_)
+			r2=$(ls $database_dir/$(basename $file | cut -d_ -f1)*R2* | grep /${sample}_)
 			samples_dir+=("${sample}")
+			echo ${sample},${r1},${r2} >> ${name} && echo ${sample},${r1},${r2}
 		fi
 	done
 	echo -e "\n"
 fi
 
+if [ ! $pooled_dir = 0 ]; then
+	samples_pooled=()
+	echo -e "Adding pooled samples"
+	for file in $pooled_dir/*.f?(ast)q*; do
+		sample=$(basename $file | cut -d_ -f 1)
+		if [[ ! " ${samples_pooled[@]} " =~ " ${sample} " ]]; then
+			p1r1=$(ls $pooled_dir/$(basename $file | cut -d_ -f1)*_*1_R1* | grep /${sample}_)
+			p1r2=$(ls $pooled_dir/$(basename $file | cut -d_ -f1)*_*1_R2* | grep /${sample}_)
+			p2r1=$(ls $pooled_dir/$(basename $file | cut -d_ -f1)*_*2_R1* | grep /${sample}_)
+			p2r2=$(ls $pooled_dir/$(basename $file | cut -d_ -f1)*_*2_R2* | grep /${sample}_)
+			samples_pooled+=("${sample}")
+			echo ${sample},${p1r1},${p1r2} >> ${name} && echo ${sample},${p1r1},${p1r2}
+			echo ${sample},${p2r1},${p2r2} >> ${name} && echo ${sample},${p2r1},${p2r2}
+		fi
+	done
+fi
