bc5ed7b2e4041033025022d9ef8863b7b09cee08
Jalees A. Nasir <jalees_nasir@hotmail.com>
2021-06-11 16:23:04 +0000

    update schema and sample table generator for fasta input

--- generate_sample_table.sh
+++ generate_sample_table.sh
@@ -7,6 +7,7 @@ shopt -s extglob
 
 database_dir=0
 vcf_dir=0
+fasta_dir=0
 name="sample_table.csv"
 existing=0
 
@@ -15,7 +16,8 @@ ASSUMES FASTQ FILES ARE NAMED AS <sample_name>_L00#_R{1,2}*.fastq(.gz)
 
 Flags:
     -d  :  Path to directory containing sample fastq(.gz) files (Absolute paths preferred for consistency, but can use relative paths) (incompatible with -f)
-    -v  :  Path to directory containing sample consensus VCF files (incompatible with -d)
+    -v  :  Path to directory containing sample consensus VCF files (incompatible with -d or -f)
+    -f  :  Path to directory containing sample consensus FASTA files (incompatible with -d or -v)
     -n  :  Name or file path for final sample table (with extension) (default: 'sample_table.csv') - will overwrite if file exists
     -e  :  Name or file path for an existing fastq(.gz) sample table - will append to the end of the provided table
 
@@ -23,23 +25,36 @@ Select one of '-n' (new sample table) or '-e' (existing sample table).
 If neither provided, a new sample table called 'sample_table.csv' will be created (or overwritten) by default. 
 """
 
-while getopts ":d:v:n:e:" option; do
+while getopts ":d:v:f:n:e:" option; do
     case "${option}" in
         d) database_dir=$OPTARG;;
         v) vcf_dir=$OPTARG;;
+        f) fasta_dir=$OPTARG;;
         n) name=$OPTARG;;
         e) existing=$OPTARG;;
     esac
 done
 
-if [ $database_dir = 0 ] && [ $vcf_dir = 0 ]; then
-    echo "You must specify a data directory containing fastq(.gz) reads (-d) or VCF files (-v)."
+if [ $database_dir = 0 ] && [ $vcf_dir = 0 ] [ $fasta_dir = 0 ]; then
+    echo "You must specify a data directory containing fastq(.gz) reads (-d),  VCF files (-v), or FASTA files (-f)."
     echo "$HELP"
     exit 1
 fi
 
-if [ ! $database_dir = 0 ] && [ ! $vcf_dir = 0 ]; then
+if [ ! $database_dir = 0 ] && [ ! $vcf_dir = 0 ] && [ ! $fasta_dir = 0 ]; then
+    echo "You can only select one directory of fastq(.gz) reads (-d), VCF files (-v), or FASTA files (-f)."
+    echo "$HELP"
+    exit 1
+elif [ ! $database_dir = 0 ] && [ ! $vcf_dir = 0 ]; then
     echo "You can only select one directory of fastq(.gz) reads (-d) or VCF files (-v)."
+    echo "$HELP"
+    exit 1
+elif [ ! $database_dir = 0 ] && [ ! $fasta_dir = 0 ]; then
+    echo "You can only select one directory of fastq(.gz) reads (-d) or FASTA files (-f)."
+    echo "$HELP"
+    exit 1
+elif [ ! $$vcf_dir = 0 ] && [ ! $fasta_dir = 0 ]; then
+    echo "You can only select one directory of VCF files (-v) or FASTA files (-f)."
     echo "$HELP"
     exit 1
 fi
@@ -51,6 +66,9 @@ if [ $existing = 0 ] && [ ! $database_dir = 0 ]; then
 elif [ $existing = 0 ] && [ ! $vcf_dir = 0 ]; then
         echo -e "Creating new sample table: ${name}\n"
         echo "sample,vcf_path" > ${name} && echo "sample,vcf_path"
+elif [ $existing = 0 ] && [ ! $fasta_dir = 0 ]; then
+        echo -e "Creating new sample table: ${name}\n"
+        echo "sample,fasta_path" > ${name} && echo "sample,fasta_path"
 else
 	filename=$(basename $existing)
 	if [ -f ""$existing"" ] ; then
@@ -93,6 +111,17 @@ if [ ! $vcf_dir = 0 ]; then
 		echo -e "\n"
 fi
 
+if [ ! $fasta_dir = 0 ]; then
+		samples_dir=()
+		samples_fail=()
+		for file in $fasta_dir/*.{fa,fna,fasta}; do
+			sample=$(basename $file | cut -d_ -f 1 | cut -d. -f1)
+			if [ -f $file ]; then
+				echo ${sample},${file} >> ${name} && echo ${sample},${file}
+			fi
+		done
+		echo -e "\n"
+fi
 
 if [ ${#samples_fail[@]} -gt 0 ]; then 
 	echo -e "Samples that failed to be added to sample table:"
