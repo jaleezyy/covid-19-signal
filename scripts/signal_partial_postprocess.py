0432825cf91c518c8be4174f0c96a5985479e2ac
Jalees A. Nasir <jalees_nasir@hotmail.com>
2021-07-30 15:15:07 +0000

    updates to postprocessing including given statistics files

--- scripts/signal_partial_postprocess.py
+++ scripts/signal_partial_postprocess.py
@@ -18,38 +18,6 @@ def check_file(path: str) -> Path:
 		return False
 		#raise argparse.ArgumentTypeError(f"{path} can't be read")
 
-def identify_stats_file(id):
-	"""
-	Determine if provided stats file is from QUAST or already provided
-	"""
-
-	files = []
-	
-	for filename in id:
-		stats = False
-		quast = False
-	
-		stats_file = os.path.join(snakemake.params['results_dir'], filename, "_stats.txt")
-		quast_file = os.path.join(snakemake.params['results_dir'], filename, "_quast_report.tsv")
-		
-		if check_file(stats_file): stats = True
-		if check_file(quast_file): quast = True
-	
-		if (stats is True) and (quast is True): # select stats file by default
-			files.append(stats_file)
-			#return stats
-		elif (stats is True) and (quast is False):
-			files.append(stats_file)
-			#return stats
-		elif (stats is False) and (quast is True):
-			files.append(quast_file)
-			#return quast
-		else:
-			files.append(stats_file)
-			#return stats
-	
-	return files
-
 def parse_lineages(file):
 	lineages = pd.read_table(file, sep='\t')
 	lin_df = lineages[['isolate', 
@@ -78,8 +46,6 @@ def parse_stats(stats):
 		else:
 			with open(file) as data:
 				file_red = data.readlines()
-				#print(file_red)
-				# exit()
 			if any(s.startswith("Assembly\t") and (match := s) for s in file_red):
 				info['isolate'].append(match.split("\t")[1].split(".")[0].strip("\n")) ### QUAST
 			else:
@@ -97,20 +63,10 @@ def parse_stats(stats):
 			else:
 				info["Genome Fraction (%)"].append(" ") ### DEFAULT
 
-
-		# for ele in file_red:
-			# if ele.startswith("Assembly\t"):
-				# info['isolate'].append(ele.split("\t")[1].split(".")[0].strip("\n"))
-			# if ele.startswith('Genome fraction (%)\t'): 
-				# info["Genome Fraction (%)"].append(ele.split("\t")[1].strip("\n"))
-			
-	
 	quast_df = pd.DataFrame(info, columns=['isolate', 'Genome Fraction (%)'])
 
 		
 	return quast_df
-	
-	
 	
 def collate_output(lineage, stats, output):
 	merged_df = lineage.merge(stats, on='isolate', how='outer')
@@ -129,7 +85,6 @@ if __name__ == '__main__':
 		raise argparse.ArgumentTypeError(f"Required lineage assessment file {args.input_lineages} can't be read!")
 	lin = parse_lineages(args.input_lineages)
 	
-	stats_files = identify_stats_file(args.input_sample)
-	stat = parse_stats(stats_files)
+	stat = parse_stats(args.input_sample)
 	
 	collate_output(lin, stat, args.output)
