c70351f09fe053599fa5fdd125b2ad9f7aca4f3f
Jalees A. Nasir <jalees_nasir@hotmail.com>
2022-02-03 18:13:28 +0000

    clean up comments

--- scripts/signal_postprocess.py
+++ scripts/signal_postprocess.py
@@ -638,7 +638,7 @@ def parse_lineage(tsv_filename, sample_names, allow_missing=True):
         if row.isolate.startswith("Consensus"):
             sid = re.findall("_(.*?)\.", row.isolate)[0]
         else:
-            sid = row.isolate
+            sid = str(row.isolate)
 
         assert sid in sample_names
 
@@ -653,32 +653,6 @@ def parse_lineage(tsv_filename, sample_names, allow_missing=True):
                          'pangolin_ver': pangolin,
                          'pangolearn_ver': pangolearn,
                          'nextclade_ver': nextclade }
-
-    # # Skip first line
-    # for line in open(tsv_filename).readlines()[1:]:
-        # n = line.split("\t")
-        # assert len(n) == 35
-
-    # # Determine sample name
-        # if n[0].startswith("Consensus"):
-            # sid = re.findall("_(.*?)\.", n[0])[0]
-        # else:
-            # sid = str(n[0])
-
-        # assert sid in sample_names
-
-    # # Pull Pangolin lineage
-        # if n[1] != '':
-            # lineage = str(n[1])
-            # clade = str(n[9])
-            # pangolin = str(n[31])
-            # pangolearn = str(n[33])
-            # nextclade = str(n[34])
-            # samples[sid] = { 'lineage' : lineage,
-                             # 'clade': clade,
-                             # 'pangolin_ver': pangolin,
-                             # 'pangolearn_ver': pangolearn,
-                             # 'nextclade_ver': nextclade }
 
     assert len(samples) == len(sample_names)
     return { 'samples': samples }
