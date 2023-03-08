5508205b2753e5afaf76b6be19442fb2396999eb
Jalees A. Nasir <jalees_nasir@hotmail.com>
2023-03-08 22:38:20 +0000

    update version number

--- signal.py
+++ signal.py
@@ -238,7 +238,7 @@ if __name__ == '__main__':
 	# note: add root_dir to determine the root directory of SIGNAL
 	script_path = os.path.join(os.path.abspath(sys.argv[0]).rsplit("/",1)[0])
 	args, allowed = create_parser()
-	version = 'v1.5.8'
+	version = 'v1.5.9'
 	alt_options = []
 	
 	if args.version:
